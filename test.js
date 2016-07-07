683dd5e11886f5e95f6067c88980d40f8326d7ec
Mark F. Ellis II <ellismarkf@gmail.com>
2016-07-07 17:29:59 -0500

    import function

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+var buildBoard = require('./matrix.js').buildBoard;
+
 describe('building the game board', function() {
   xit('should build a two-dimensional array of tile objects with 9 rows and columns by default', function() {
 
